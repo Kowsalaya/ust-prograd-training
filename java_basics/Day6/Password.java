c1a496565fa60db74a959fd3a2f7573e66b2a42e
Kowsalaya <17euit161@skcet.ac.in>
2021-11-30 08:55:15 +0530

    Java basics added

---
+++ java_basics/Day6/Password.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Password {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+…           try {
+            try {
+                int x=new Integer(sc.nextInt());
+                int y=new Integer(sc.nextInt());
+                System.out.println(""+(x/y));
+            }
+            catch (InputMismatchException e) {
+                System.out.print("java.util.InputMismatchException");
+            }
+        }catch(ArithmeticException e)
+        {
+            System.out.println("java.lang.ArithmeticException: / by zero");
+        }
+    }
+}
